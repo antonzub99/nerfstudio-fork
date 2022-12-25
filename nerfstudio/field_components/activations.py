fix for numerical stability

nerfstudio/field_components/activations.py
@@ -34,7 +34,7 @@ class _TruncExp(Function):  # pylint: disable=abstract-method
     @custom_bwd
     def backward(ctx, g):  # pylint: disable=arguments-differ
         x = ctx.saved_tensors[0]
-        return g * torch.exp(x.clamp(-15, 15))
+        return g * torch.exp(x.clamp(-12, 12))
 
 
 trunc_exp = _TruncExp.apply